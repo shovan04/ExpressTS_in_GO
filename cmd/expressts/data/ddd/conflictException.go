666d5b2f05ebf27bd7db284ba8107b71d0d805cf
Shovan04 <shovanm50@gmail.com>
2026-01-11 18:00:03 +0530

    feat(ddd): add conflict exception

---
+++ cmd/expressts/data/ddd/conflictException.go
@@ -0,0 +1,26 @@
+package ddd
+
+func GetConflictExceptionContent() []byte {
+	return []byte(`import HttpResponseCode from "../../constants/httpResponseCode.js";
+
+class ConflictException extends Error {
+  statusCode: number;
+  code: string;
+  override cause?: unknown;
+
+  constructor(message: string, cause?: unknown) {
+    super(message);
+
+    this.name = "ConflictException";
+    this.code = "CONFLICT_ERROR";
+    this.statusCode = HttpResponseCode.CONFLICT;
+    this.cause = cause;
+
+    Object.setPrototypeOf(this, new.target.prototype);
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+}
+
+export default ConflictException;
+`)
+}
