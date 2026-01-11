e0f9787e0e117eaece68d34ca96897c859635dd5
Shovan04 <shovanm50@gmail.com>
2026-01-11 17:20:11 +0530

    feat: add UserController implementation for user creation endpoint

---
+++ cmd/expressts/data/ddd/userController.go
@@ -0,0 +1,23 @@
+package ddd
+
+func GetUserControllerContent() []byte {
+	return []byte(`
+import { Request, Response } from "express";
+import { CreateUserUseCase } from "../../../application/use-cases/CreateUserUseCase.js";
+import { CreateUserDTO } from "../../../application/dtos/CreateUserDTO.js";
+
+export class UserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  public async create(req: Request, res: Response): Promise<void> {
+    try {
+      const dto: CreateUserDTO = req.body;
+      const user = await this.createUserUseCase.execute(dto);
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  }
+}
+`)
+}
