013714b8409ee1fb911c3961eead18aa15d7915f
Shovan04 <shovanm50@gmail.com>
2026-01-11 17:19:55 +0530

    feat: add IUserRepository interface definition for user data operations

---
+++ cmd/expressts/data/ddd/userRepositoryInterface.go
@@ -0,0 +1,13 @@
+package ddd
+
+func GetUserRepositoryInterfaceContent() []byte {
+	return []byte(`
+import { User } from "../entities/User.js";
+
+export interface IUserRepository {
+  save(user: User): Promise<void>;
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+}
+`)
+}
